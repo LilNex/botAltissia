27255720d188e0329bd0a12957482ed907934218
LilNex <nextorchq@gmail.com>
2022-02-20 09:21:41 +0100

    classes

--- classes.py
+++ classes.py
@@ -2,15 +2,8 @@
 from selenium.webdriver.remote.webelement import *
 from selenium import *
 from typing import List
-vector:List[float] = list()
 
 
-
-class Theme:
-  listCards=[]
-  def __init__(self, name:str, childs:WebElement):
-    self.name = name
-    self.childs = childs
 
 
 class cardTheme:
@@ -19,6 +12,24 @@ class cardTheme:
     self.childs = childs
   def click(self):
     self.childs.click()
+class Theme:
+  # listCards=[]
+  listCards:List[cardTheme] = list()
+
+  def __init__(self, name:str, childs:WebElement):
+    self.name = name
+    self.childs = childs
+
+  def getCountExCards(self):
+    return len(self.childs.find_elements_by_class_name('mission-lesson-item'))
+
+
+class question:
+  def __init__(self, num:int):
+    self.num = num
+    self.reponses = []
+
+
 
 def elementHasClass(element, active):
     return element.getAttribute("class").contains(active)
