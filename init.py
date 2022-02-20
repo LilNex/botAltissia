ecadfa256c6a100acd75fa2b5b3e002f5c44dd03
LilNex <nextorchq@gmail.com>
2022-02-20 20:38:46 +0100

    .

--- init.py
+++ init.py
@@ -19,8 +19,8 @@ browser = webdriver.Chrome(executable_path='chromedriver.exe')
 
 
 # System.setProperty("webdriver.gecko.driver",Path_of_Firefox_Driver"); // Setting system properties of FirefoxDriver
-driver = webdriver.Firefox()()
-
+# driver = webdriver.Firefox()()
+# 
 
 
 # ============= Setting =============
